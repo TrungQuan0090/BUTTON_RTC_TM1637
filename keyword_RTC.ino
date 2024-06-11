ce43953c7c3ec59fc61004cb8666dd683a6aad3f
Trung Quan <0308221160@caothang.edu.vn>
2024-06-11 10:46:20 +0700

    Update keyword_RTC.ino

--- keyword_RTC.ino
+++ keyword_RTC.ino
@@ -1,14 +1,17 @@
 #include <RTClib.h>
 
 RTC_DS3231 rtc;
+
 void setup(){
-if (! rtc.begin()) {
+
+  if (! rtc.begin()) {
   Serial.println("Couldn't find RTC");
   while (1);
-}
+    
+  }
 }
 void loop()
 {
 
-
+...
 }
