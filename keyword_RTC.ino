8c9533e353a0d137ae71f2ea4fe0be5c33973f2a
Trung Quan <0308221160@caothang.edu.vn>
2024-06-11 12:30:08 +0700

    Update keyword_RTC.ino

--- keyword_RTC.ino
+++ keyword_RTC.ino
@@ -7,11 +7,21 @@ void setup(){
   if (! rtc.begin()) {
   Serial.println("Couldn't find RTC");
   while (1);
+  
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     
   }
 }
 void loop()
 {
-
-...
+  DateTime now = rtc.now();
+  
+  long ngay = now.day();
+  long thang = now.month();
+  long nam = now.year();
+  
+  long gio = now.hour();
+  long phut = now.minute();
+  long giay = now.second();
+  
 }
